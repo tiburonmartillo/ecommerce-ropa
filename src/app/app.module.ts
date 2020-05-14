c6cd5eceffe9581000031832b116cbe5bec8bd7f
tiburonmartillo <sivillanueva@outlook.com>
2020-05-14 08:36:09 -0500

    Practica wix con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { FooterComponent } from './components/share/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
