21d90a0e998020ac188a544f7553b7f0d30efc88
tiburonmartillo <sivillanueva@outlook.com>
2020-05-14 10:25:03 -0500

    HEADER TERMINADO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/share/navbar/navbar.component';
 import { FooterComponent } from './components/share/footer/footer.component';
+import { HeaderComponent } from './components/share/header/header.component';
+import { SliderPromosComponent } from './components/share/slider-promos/slider-promos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    SliderPromosComponent,
+ 
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/share/slider-promos/slider-promos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-promos',
+  templateUrl: './slider-promos.component.html',
+  styleUrls: ['./slider-promos.component.css']
+})
+export class SliderPromosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
